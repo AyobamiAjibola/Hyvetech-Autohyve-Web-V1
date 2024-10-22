5983dff05e58b6efe9031f4c9590a475ee34504a
AyobamiAjibola <ayobamiajibola46@gmail.com>
2024-10-22 22:02:55 +0100

    ui-design

--- src/pages/auth/Hyvepay.tsx
+++ src/pages/auth/Hyvepay.tsx
@@ -184,6 +184,7 @@ const Hyvepay = () => {
                                   ? setShowIndividualAccountModal(true)
                                   : setShowAccountModal(true)
                           }}
+                  disabled={true}
                 />
               )}
 
